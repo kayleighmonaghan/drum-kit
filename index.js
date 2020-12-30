bfdd32538210cfc77d373f33b4bbb21c1f448ad2
Kayleigh <55881407+kayleighmonaghan@users.noreply.github.com>
2020-12-30 23:23:26 +0000

    Some CSS and some JS, working along to tutorial vid

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const keys = document.querySelector('.key');
+
 window.addEventListener('keydown', function(event) {
     const audio = document.querySelector(`audio[data-key='${event.key}']`);
-    if(!audio) return; // stop the function from running altogether
+    const key = document.querySelector(`.key[data-key='${event.key}']`);
+
+    if(!audio) return; // stop the function from running all together
     audio.currentTime = 0; // stops waiting for sound to end before other can be played
     audio.play();
-})
+    key.classList.add('playing');
+});
+
+function removeTransition(event) {
+if (event.propertyName !== 'transition') return; // skip it if it's not a transform
+this.classList.remove('playing');
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
