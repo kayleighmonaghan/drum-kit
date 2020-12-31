919cbd859101158d1ed47b7466880e7f1122df43
Kayleigh <55881407+kayleighmonaghan@users.noreply.github.com>
2020-12-31 14:57:41 +0000

    Cleaning up

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 function playSound(event) {
     const audio = document.querySelector(`audio[data-key='${event.key}']`);
     const key = document.querySelector(`.key[data-key='${event.key}']`);
-
     if(!audio) return; // stops the function from running if key with no audio element is pressed
-    
     audio.currentTime = 0; // stops waiting for sound to end before other can be played
-    
     audio.play();
-    
     key.classList.add('playing');
 };
 
 function removeTransition(event) {
-    if(event.propertyName !== 'transform') return;
+    if(event.propertyName !== 'transform') return; // ignores all transitions except for 'transofrm'
     this.classList.remove('playing');
 }
 
